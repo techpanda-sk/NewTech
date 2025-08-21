e8858af82c453b840eeb876bdb46231517bbde41
techpanda-sk <s.k.suraj5516@gmail.com>
2025-08-21 11:03:34 +0530

    hasgjh

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -6,7 +6,7 @@ import Login from "../pages/admin/components/auth/LoginForm";
 import LayoutProvider from "../pages/admin/components/layout/LayoutProvider";
 import View from "../pages/admin/components/pages/action/View";
 import { AdminProvider } from "../pages/admin/components/context/AdminContext";
-import MSMEAwardApplicationForm from "../pages/frontend/components/pages/MSMEApplicationPage";
+// import MSMEAwardApplicationForm from "../pages/frontend/components/pages/MSMEApplicationPage";
 
 const AppRoutes = () => {
   const [selectedMember, setSelectedMember] = useState(null);
@@ -16,25 +16,25 @@ const AppRoutes = () => {
     <div>
       <Router>
         <LayoutProvider>
-          {/* <AdminProvider> */}
-            <View
-              {...{ showMemberDialog, setShowMemberDialog, selectedMember }}
-            />
-            <Routes>
-                  {/* Frontend Routes */}
-                  <Route path="/" element={<MSMEAwardApplicationForm/>}/>
-                   {/* Admin Panel Routes */}
-              <Route path="admin/login" element={<Login />} />
-              <Route path="/admin" element={<Layout />}>
-                <Route
-                  path="table"
-                  element={
-                    <Table {...{ setSelectedMember, setShowMemberDialog }} />
-                  }
-                />
-              </Route>
-            </Routes>
-          {/* </AdminProvider> */}
+          <AdminProvider>
+          <View
+            {...{ showMemberDialog, setShowMemberDialog, selectedMember }}
+          />
+          <Routes>
+            {/* Frontend Routes */}
+            {/* <Route path="/" element={<MSMEAwardApplicationForm />} /> */}
+            {/* Admin Panel Routes */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Layout />}>
+              <Route
+                path="table"
+                element={
+                  <Table {...{ setSelectedMember, setShowMemberDialog }} />
+                }
+              />
+            </Route>
+          </Routes>
+          </AdminProvider>
         </LayoutProvider>
       </Router>
     </div>
